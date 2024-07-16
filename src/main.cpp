94a183a5ae32d9b067444a4a157b0bc93bd34850
wellsilver <wellsilver2016@gmail.com>
2024-07-16 17:50:03 -0400

    naw its windows only now :skull:

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#define CL_TARGET_OPENCL_VERSION 120
+#include <CL/cl.h>
+#include "shader.hpp"
 
-void main() {
+bool closing = false;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+  // Register the window class.
+  const wchar_t CLASS_NAME[]  = L"Sample Window Class";
   
+  WNDCLASS wc = {};
+  wc.lpfnWndProc   = WindowProc;
+  wc.hInstance     = hInstance;
+  wc.lpszClassName = CLASS_NAME;
+
+  RegisterClass(&wc);
+
+  // Create the window.
+
+  HWND hwnd = CreateWindowEx(
+    0,                              // Optional window styles.
+    CLASS_NAME,                     // Window class
+    L"Hgame",    // Window text
+    WS_OVERLAPPEDWINDOW,            // Window style
+
+    // Size and position
+    CW_USEDEFAULT, CW_USEDEFAULT, 480, 480,
+
+    NULL,       // Parent window    
+    NULL,       // Menu
+    hInstance,  // Instance handle
+    NULL        // Additional application data
+  );
+
+  if (hwnd == NULL) return -1;
+  
+  ShowWindow(hwnd, nCmdShow);
+
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0) > 0) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+  switch (uMsg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+    case WM_PAINT:
+      return 0;
+    case WM_SIZE:
+      return 0;
+  }
+  return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
